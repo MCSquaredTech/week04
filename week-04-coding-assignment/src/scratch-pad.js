678c20c562fb32ddeb3d856f13f0a6f50d9e6875
Mark Christensen <mark@mcsquared.tech>
2023-05-01 20:41:35 -0600

    07 Completed Work for Week 04

--- week-04-coding-assignment/src/scratch-pad.js
+++ week-04-coding-assignment/src/scratch-pad.js
@@ -1,6 +1,15 @@
 // Create array licenseNumers 
 let licenseNumbers = ["ZF1 Y1AQ", "WF3 CSIU", "NMB K4ID", "FO1 3IEP", "XYQ XXOA", "YHL J7TR", "A4I MR5T", "OIC U812", "NMC F4ID", "NMA 4KID", "NMA 4KED"];
 
+let sortByLicenseSimpleSort = (a) => {
+    return a.sort((a,b) => { 
+        return a-b;
+    })
+};
+
+console.log(sortByLicenseSimpleSort(licenseNumbers));
+
+
 // Matt Zoymota's solution 
 let sortByLicensePlate = (a) => { 
     return a.sort((a,b) => {
