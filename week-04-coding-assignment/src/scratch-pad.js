43b811fbbc8242c2d7022563ad73d09f00e30cad
Mark Christensen <mark@mcsquared.tech>
2023-05-01 19:43:50 -0600

    06 Added PDF and Update Word Document

---
+++ week-04-coding-assignment/src/scratch-pad.js
@@ -0,0 +1,27 @@
+// Create array licenseNumers 
+let licenseNumbers = ["ZF1 Y1AQ", "WF3 CSIU", "NMB K4ID", "FO1 3IEP", "XYQ XXOA", "YHL J7TR", "A4I MR5T", "OIC U812", "NMC F4ID", "NMA 4KID", "NMA 4KED"];
+
+// Matt Zoymota's solution 
+let sortByLicensePlate = (a) => { 
+    return a.sort((a,b) => {
+        if (a < b) {
+            return -1;
+        } else if (a > b) { 
+            return 1;
+        }
+        return 0
+    })
+}
+
+console.log(sortByLicensePlate(licenseNumbers));
+
+
+// My solution
+let sortByLicenseNumber = (a) => { 
+    return a.sort((a,b) => { 
+        return a < b ? -1 : 1;
+    })
+}
+
+console.log(sortByLicenseNumber(licenseNumbers));
+
